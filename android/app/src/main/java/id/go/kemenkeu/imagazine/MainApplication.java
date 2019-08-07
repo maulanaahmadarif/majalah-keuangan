37c7681b3c7ad3c9ae74229142184166b73ccbb8
Arif Maulana <arif.maulana@ovo.id>
2019-08-07 10:47:35 +0700

    change android app name

--- android/app/src/main/java/id/go/kemenkeu/imagazine/MainApplication.java
+++ android/app/src/main/java/id/go/kemenkeu/imagazine/MainApplication.java
@@ -46,7 +46,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNDeviceInfo(),
+          new RNDeviceInfo(),
           new AsyncStoragePackage(),
           new SQLitePluginPackage(),
           new FBSDKPackage(mCallbackManager),
